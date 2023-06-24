0cf0d1938f8bf5b3ca4577945472d1836d2172e3
chika nwaigwe goodluck <chikagnwaigwe@gmail.com>
2023-06-24 06:31:02 -0700

    tasks done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
 # install flask-pip3 -v 2.1.0
 
 exec { 'flask-pip3':
-  command => '/usr/bin/apt-get -y install flask-pip3 -v 2.1.0',
+  command => '/usr/bin/apt-get -y gem install flask-pip3 -v 2.1.0',
 }
